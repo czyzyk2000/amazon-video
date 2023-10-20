6a2fa2be4b510bde668888e33f2be88c7d4cfbce
Patryk Czyzkowski <patryk.czyzkowskir1@gmail.com>
2023-10-20 15:32:15 +0200

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <Route exact path="/">
             <h1>test</h1>
             <h2>test2</h2>
+            <h3>test3</h3>
             <Login />
           </Route>
         </Switch>
